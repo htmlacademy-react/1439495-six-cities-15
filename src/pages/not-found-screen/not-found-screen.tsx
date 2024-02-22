bb1b4979fb68dcdadd71837629e3773495e7f308
Anna <anna.mironova89@yandex.ru>
2024-02-22 12:07:51 +0300

    Изменила компонент для 404 страницы

--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
-import { AppRoutes } from '../../const';
+import { AppRoutes } from '../../const.ts';
+import './not-found-screen.css';
 
-function NotFoundScreen() {
+function NotFoundScreen(): JSX.Element {
   return (
-    <div>
-      <p>Ошибка 404. Запрашиваемая страница не найдена</p>
-      <Link to={AppRoutes.Main}>Вернуться на главную страницу</Link>
+    <div className="body">
+      <section id="not-found">
+        <div id="title"><Link to={AppRoutes.Main}>Вернуться на главную</Link> &bull; 404 Error Page</div>
+        <div className="circles">
+          <p>404<br />
+            <small>PAGE NOT FOUND</small>
+          </p>
+          <span className="circle big"></span>
+          <span className="circle med"></span>
+          <span className="circle small"></span>
+        </div>
+      </section>
     </div>
   );
 }
