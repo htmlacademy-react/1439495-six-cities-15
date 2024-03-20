4df3d06f24453871deca42aa5dab89b18c8999a3
Anna <anna.mironova89@yandex.ru>
2024-03-20 21:43:57 +0300

    Добавила модуль для работы с токеном

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BASE_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const TIMEOUT = 5000;
@@ -9,5 +10,16 @@ export const createAPI = (): AxiosInstance => {
     timeout: TIMEOUT
   });
 
+  api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['X-Token'] = token;
+      }
+
+      return config;
+    }
+  );
   return api;
 };
