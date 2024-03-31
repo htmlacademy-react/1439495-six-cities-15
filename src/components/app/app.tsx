85b68c99effe69d972ae4a194a998345aaa7bcd2
Anna <anna.mironova89@yandex.ru>
2024-03-31 17:17:38 +0300

    Исправила баг в PrivateRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks/store-hooks.ts';
+import { useAppSelector } from '../../hooks/store-hooks.ts';
 import MainScreen from '../../pages/main-screen/main-screen.tsx';
-import { AppRoutes, AuthorizationStatus } from '../../const.ts';
+import { AppRoutes } from '../../const.ts';
 import LoginScreen from '../../pages/login-screen/login-screen.tsx';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen.tsx';
 import OfferScreen from '../../pages/offer-screen/offer-screen.tsx';
@@ -10,26 +9,11 @@ import NotFoundScreen from '../../pages/not-found-screen/not-found-screen.tsx';
 import PrivateRoute from '../private-route/private-route.tsx';
 import ScrollToTop from '../scroll-to-top/scroll-to-top.tsx';
 import Layout from '../layout/layout.tsx';
-import { checkAuthStatus, fetchCards, fetchFavoriteCards } from '../../store/api-actions.ts';
 import LoadingSpinner from '../loading-spinner/loading-spinner.tsx';
 import { getCardsLoadingStatus } from '../../store/cards/cards-selectors.ts';
-import { getAuthorizationStatus } from '../../store/user/user-selectors.ts';
 
 function App(): JSX.Element {
-  const dispatch = useAppDispatch();
   const isLoading = useAppSelector(getCardsLoadingStatus);
-  const authorizationStatus = useAppSelector(getAuthorizationStatus);
-
-  useEffect(() => {
-    dispatch(fetchCards());
-    dispatch(checkAuthStatus());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (authorizationStatus === AuthorizationStatus.Auth) {
-      dispatch(fetchFavoriteCards());
-    }
-  }, [dispatch, authorizationStatus]);
 
   if (isLoading) {
     return <LoadingSpinner />;
--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
-import { AppRoutes, AuthorizationStatus } from '../../const.ts';
+import { AppRoutes } from '../../const.ts';
 import { useAppSelector } from '../../hooks/store-hooks.ts';
-import { getAuthorizationStatus } from '../../store/user/user-selectors.ts';
+import { getUserInfo } from '../../store/user/user-selectors.ts';
 
 type PrivateRouteProps = {
   children: JSX.Element;
@@ -9,12 +9,16 @@ type PrivateRouteProps = {
 }
 
 function PrivateRoute({children, isReverse}: PrivateRouteProps): JSX.Element {
-  const authorizationStatus = useAppSelector(getAuthorizationStatus);
-  return (
-    authorizationStatus === (isReverse ? AuthorizationStatus.NoAuth : AuthorizationStatus.Auth) ?
-      children :
-      <Navigate to={isReverse ? AppRoutes.Main : AppRoutes.Login} />
-  );
+
+  const user = useAppSelector(getUserInfo);
+  if (user && isReverse) {
+    return <Navigate to={AppRoutes.Main} />;
+  }
+  if (!user && !isReverse) {
+    return <Navigate to={AppRoutes.Login} />;
+  }
+
+  return children;
 }
 
 export default PrivateRoute;
